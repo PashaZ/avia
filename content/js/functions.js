61c15451d207fba843395e6307e8a2d449dc93c9
Pasha <pavlo.zakernychnyy@gmail.com>
2023-01-05 19:20:58 +0200

    finished right scroll

--- content/js/functions.js
+++ content/js/functions.js
@@ -37,9 +37,7 @@ $(document).ready(function(){
         console.log("hello555")
             	
 });
-document.querySelector("scroll-top"){
-  console.log("hello")  
-}
+
 
 
 
@@ -48,8 +46,66 @@ document.querySelector("scroll-top"){
 $(document).click(function(event) {      	
    	$('.cover').click(
 		function() {
-            $('.header nav, .header nav ul.dropdown').hide();
-            $('.header nav span.with-angle, .menu-btn').removeClass('active');
+            $(' nav,  nav ul.dropdown').hide();
+            $(' nav span.with-angle, .menu-btn').removeClass('active');
             $('.cover').fadeOut();
             	});
 });
+
+// ------------------------------------------------
+
+// aside-scroll.js-----------------------------------------------------
+window.onscroll = () => {
+	const scrollBlock = document.getElementById('offer-card-fixed');
+	const position = window.scrollY;
+
+	if ((770 < position) && (position < 4300)) {
+		scrollBlock.classList.add('fixed');
+		scrollBlock.classList.remove('fixed-to-bottom');
+
+	}
+	else if (position < 770) {
+		scrollBlock.classList.remove('fixed');
+		scrollBlock.classList.remove('fixed-to-bottom');
+
+	}
+	else if (position >= 4300) {
+		scrollBlock.classList.remove('fixed');
+		scrollBlock.classList.add('fixed-to-bottom');
+
+	}
+}
+
+
+// header-flags.js------------------------------------------
+(() => {
+	const refs = {
+		openCloseModalBtn: document.querySelector('.header-flags-button'),
+		changeArrow: document.querySelector('.button-arrow'),
+		modal: document.querySelector('.header-flags-section'),
+	};
+
+	refs.openCloseModalBtn.addEventListener('click', toggleModalBtn);
+
+	function toggleModalBtn() {
+		refs.modal.classList.toggle('is-hidden');
+		refs.changeArrow.classList.toggle('change-arrow');
+	}
+})();
+ 
+// скрол 
+
+(() => {
+	const refs = {
+		openCloseModalBtn: document.querySelector('.header-flags-button'),
+		changeArrow: document.querySelector('.button-arrow'),
+		modal: document.querySelector('.header-flags-section'),
+	};
+
+	refs.openCloseModalBtn.addEventListener('click', toggleModalBtn);
+
+	function toggleModalBtn() {
+		refs.modal.classList.toggle('is-hidden');
+		refs.changeArrow.classList.toggle('change-arrow');
+	}
+})();
